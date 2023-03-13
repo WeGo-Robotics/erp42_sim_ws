ccaa81fbacb7fd3fb0a9e8271ed8a23947dcbf85
donggeon-choi <127081455+donggeon-choi@users.noreply.github.com>
2023-03-13 17:00:34 +0900

    Update 13.HSV_trackbar.py

--- src/opencv_edu/scripts/13.HSV_trackbar.py
+++ src/opencv_edu/scripts/13.HSV_trackbar.py
@@ -10,22 +10,30 @@ import numpy as np
 
 class HSV_trackbar:
     def __init__(self):
+        # Initialize the ROS node with a unique name
         rospy.init_node("image_node", anonymous=False)
+        # Publisher for the converted image
         self.image_cvt_pub = rospy.Publisher("cvt_img", Image, queue_size=10)
         self.rate = rospy.Rate(10)  # 10Hz
 
+        # Create an instance of the rospack object
         rospack = rospkg.RosPack()
+        # Get the path to the package containing the script
         self.file_path = rospack.get_path("opencv_edu")
+        # Append the script directory to the file path
         self.file_path += "/scripts/"
 
+        # The path to the HSV image file
         self.hsv_path = self.file_path + "HSV.png"
+        # The name of the window for the trackbar GUI
         self.win_name = "color_detect_hsv"
+        # Create the trackbars for the HSV ranges
         self.create_trackbar_init()
 
     def create_trackbar_init(self):
-        # Trackbar를 표시할 윈도우 이름을 명시합니다.
+        # Create a named window for the trackbar GUI
         cv2.namedWindow(self.win_name, cv2.WINDOW_NORMAL)
-        # Trackbar의 변수명, 윈도우 이름과 변수 범위를 설정합니다.
+        # Create trackbars for the HSV range
         cv2.createTrackbar("LH", self.win_name, 0, 179, self.hsv_track)
         cv2.createTrackbar("LS", self.win_name, 0, 255, self.hsv_track)
         cv2.createTrackbar("LV", self.win_name, 0, 255, self.hsv_track)
@@ -34,7 +42,7 @@ class HSV_trackbar:
         cv2.createTrackbar("UV", self.win_name, 255, 255, self.hsv_track)
 
     def hsv_track(self, value):
-        # Trackbar의 조절한 값을 변수에 저장합니다.
+        # Get the values of the trackbars
         self.L_H_Value = cv2.getTrackbarPos("LH", self.win_name)
         self.L_S_Value = cv2.getTrackbarPos("LS", self.win_name)
         self.L_V_Value = cv2.getTrackbarPos("LV", self.win_name)
@@ -43,15 +51,21 @@ class HSV_trackbar:
         self.U_V_Value = cv2.getTrackbarPos("UV", self.win_name)
 
     def main(self):
+        # Load the HSV image file
         self.color_img = cv2.imread(self.hsv_path, cv2.IMREAD_COLOR)
+        # Convert the BGR image to HSV
         self.cvt_hsv = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2HSV)
+        # Define the HSV range using the trackbar values
         self.lower = np.array([self.L_H_Value, self.L_S_Value, self.L_V_Value])
         self.upper = np.array([self.U_H_Value, self.U_S_Value, self.U_V_Value])
+        # Create a mask using the HSV range
         self.mask = cv2.inRange(self.cvt_hsv, self.lower, self.upper)
+        # Apply the mask to the original image to obtain the final result
         self.rst = cv2.bitwise_and(self.color_img, self.color_img, mask=self.mask)
-        # np array를 string으로 변경합니다. (디스플레이용)
+        # Convert np array to string for display purposes
         lower_string = "lower H,S,V : " + ",".join(str(e) for e in self.lower.tolist())
         upper_string = "upper H,S,V : " + ",".join(str(e) for e in self.upper.tolist())
+        # Add text to the image
         cv2.putText(
             self.rst,
             lower_string,
@@ -70,13 +84,16 @@ class HSV_trackbar:
             (255, 255, 255),
             3,
         )
+        # Display the image in a window
         cv2.imshow(self.win_name, self.rst)
-
+        # Wait for a key press
         cv2.waitKey(1)
         # Initialize bridge to convert between ROS and OpenCV images
         self.bridge = CvBridge()
+        # Convert the masked image to an Image message and publish it to the "cvt_img" topic
         image_msg = self.bridge.cv2_to_imgmsg(self.rst, "bgr8")
         self.image_cvt_pub.publish(image_msg)
+        # Sleep to maintain the rate of the node
         self.rate.sleep()
 
 
