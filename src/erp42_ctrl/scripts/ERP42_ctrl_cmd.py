6cb25fdac233100976c40ce80c78d0485b03cd39
donggeon-choi <127081455+donggeon-choi@users.noreply.github.com>
2023-03-13 15:25:29 +0900

    Update ERP42_ctrl_cmd.py

--- src/erp42_ctrl/scripts/ERP42_ctrl_cmd.py
+++ src/erp42_ctrl/scripts/ERP42_ctrl_cmd.py
@@ -5,34 +5,55 @@ from morai_msgs.msg import CtrlCmd
 from math import *
 from time import *
 
-
+# Define ERP_ctrl_cmd class
 class ERP42_ctrl_cmd:
+    # ERP_ctrl_cmd class constructor
     def __init__(self):
+        # Initialize ROS node
         rospy.init_node("erp42_ctrl_cmd_node")
+        # Save Start time
         self.start_time = rospy.get_time()
+        # Create a publisher that publishes CtrlCmd type messages to "/ctrl_cmd" topic
         self.erp42_ctrl_pub = rospy.Publisher("/ctrl_cmd", CtrlCmd, queue_size=3)
+        # Steering limit value
         self.steering_bound = 28.2
-
+        
+    # main function
     def main(self):
+        # Create a CtrlCmd message object
         self.ctrl_cmd_msg = CtrlCmd()
+        # Save the current time
         self.second_time = rospy.get_time()
-
+        
+        # Set speed  0 ~ 23.8 -> 0 ~ 1
         speed = 15  # 0 ~ 23.8 -> 0 ~ 1
+        # Adjust speed value to within the range of 0 ~ 23.8
         speed = max(0, min(speed, 23.8))
+        # Steering angle (left: positive, right: negative)
         steering_deg = 10
+        # Adjust within the range of steering limit values
         steering_deg = max(-self.steering_bound, min(steering_deg, self.steering_bound))
+        # Change angle to radians
         steering_rad = steering_deg * pi / 180
+        # Since the sign of the ERP42 control message and the steering angle are opposite, reverse the sign.
         steering_deg = -steering_deg
+        # Save the speed value in the message object
         self.ctrl_cmd_msg.accel = speed
+        # Save the steering value in the message object
         self.ctrl_cmd_msg.steering = steering_rad
-
+        
+        # Update speed and steering values every 3 seconds and publish them
         if self.second_time - self.start_time >= 3:
+            # Update the start time
             self.start_time = self.second_time
+            # Publish the message object to the "/ctrl_cmd" topic.
             self.erp42_ctrl_pub.publish(self.ctrl_cmd_msg)
 
-
+            
+# When the main script is executed, run the following code.
 if __name__ == "__main__":
     try:
+        # Create an object of the ERP42_ctrl_cmd class
         erp42_ctrl_cmd = ERP42_ctrl_cmd()
 
         while not rospy.is_shutdown():
