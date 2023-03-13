45a913076f435e7e00f42e3e627de5964dd3acd6
donggeon-choi <127081455+donggeon-choi@users.noreply.github.com>
2023-03-13 16:40:28 +0900

    Update 9.cvt_color.py

--- src/opencv_edu/scripts/9.cvt_color.py
+++ src/opencv_edu/scripts/9.cvt_color.py
@@ -7,28 +7,37 @@ import cv2
 from cv_bridge import CvBridge
 import numpy as np
 
-
+# Define a class called CvtColor
 class CvtColor:
+    # Constructor method
     def __init__(self):
+        # Initialize the node and the Publisher and Rate settings
         rospy.init_node("image_node", anonymous=False)
+        # Create a publisher to publish the image to the "cvt_img" topic
         self.image_cvt_pub = rospy.Publisher("cvt_img", Image, queue_size=10)
         self.rate = rospy.Rate(10)  # 10Hz
 
+        # Set the image path
         rospack = rospkg.RosPack()
         self.file_path = rospack.get_path("opencv_edu")
         self.file_path += "/scripts/"
 
+        # Set the image file name
         self.wego_path = self.file_path + "wego.png"
 
+    # Main method
     def main(self):
+        # Load the image
         self.color_img = cv2.imread(self.wego_path, cv2.IMREAD_COLOR)
         self.gray_img = cv2.imread(self.wego_path, cv2.IMREAD_GRAYSCALE)
 
+        # Convert the image
         self.cvt_gray = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2GRAY)
         self.cvt_hsv = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2HSV)
         self.cvt_hsl = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2HLS)
         self.cvt_lab = cv2.cvtColor(self.color_img, cv2.COLOR_BGR2Lab)
 
+        # Combine the images
         self.gray_3d = np.dstack((self.cvt_gray, self.cvt_gray, self.cvt_gray))
         self.cvt1 = cv2.hconcat([self.gray_3d, self.cvt_lab])
         self.cvt2 = cv2.hconcat([self.cvt_hsv, self.cvt_hsl])
@@ -38,6 +47,7 @@ class CvtColor:
         # Initialize bridge to convert between ROS and OpenCV images
         self.bridge = CvBridge()
         image_msg = self.bridge.cv2_to_imgmsg(self.dst, "bgr8")
+        # Publish the image message
         self.image_cvt_pub.publish(image_msg)
         self.rate.sleep()
 
