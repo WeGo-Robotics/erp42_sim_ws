9aee96558115114cf995c65749e90ae10fbfb5af
donggeon-choi <127081455+donggeon-choi@users.noreply.github.com>
2023-03-13 15:01:49 +0900

    Update ERP42_IMU.py

--- src/erp42_ctrl/scripts/ERP42_IMU.py
+++ src/erp42_ctrl/scripts/ERP42_IMU.py
@@ -5,22 +5,28 @@ from sensor_msgs.msg import Imu
 import os
 
 
+# Define IMU class
 class IMU:
+    # IMU class constructor
     def __init__(self):
+        # Initialize ROS node
         rospy.init_node("imu_node")
+        # Create a subscriber that listens to messages on "/imu" topic and sets the callback function to imu_CB.
         self.imu_sub = rospy.Subscriber("/imu", Imu, self.imu_CB)
 
-    # imu 정보 콜백 함수
+    # Callback function called when receiving messages from subscriber
     def imu_CB(self, msg):
+        # Create a message object
         self.imu_msg = Imu()
+        # Store the received msg in traffic_msg
         self.imu_msg = msg
 
-        os.system("clear")
+        os.system("clear")  # Clear the terminal
         print(f"------------------------------")
         print(f"angular_velocity :")
-        print(f"    x : {self.imu_msg.angular_velocity.x}")
-        print(f"    y : {self.imu_msg.angular_velocity.y}")
-        print(f"    z : {self.imu_msg.angular_velocity.z}")
+        print(f"    x : {self.imu_msg.angular_velocity.x}")  # x
+        print(f"    y : {self.imu_msg.angular_velocity.y}")  # y
+        print(f"    z : {self.imu_msg.angular_velocity.z}")  # z
         print(f"angular_velocity :")
         print(f"    x : {self.imu_msg.angular_velocity.x}")
         print(f"    y : {self.imu_msg.angular_velocity.y}")
@@ -32,6 +38,8 @@ class IMU:
         print(f"------------------------------")
 
 
+# When the main script is executed, run the following code.
 if __name__ == "__main__":
+    # Create IMU class object
     imu = IMU()
     rospy.spin()
